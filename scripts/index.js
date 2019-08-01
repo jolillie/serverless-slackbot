7f5382b66e0e3b9ffb47d8f6fa07bfe953a2ef15
kiramrice <kiramrice@gmail.com>
2019-08-01 17:57:50 -0400

    index in progress

--- scripts/index.js
+++ scripts/index.js
@@ -5,21 +5,4 @@
 var json = require('./answers.json');
 module.exports = bot => {
 
-  // responds to any slash command
-  bot.on('slash_command', payload => {
-
-    bot.reply(payload, {
-      text: " "
-    })
-  })
-
-  // responds to keyword in a text
-  bot.hears(/ /, (payload, bot, match) => {
-
-    bot.reply(payload, {
-      text: JSON.stringify(json)
-    })
-  })
-
-
-
+ xxxx
